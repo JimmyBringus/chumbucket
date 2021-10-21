acd233143296d3a20a7746a86cc136654c15bd87
Jimmy B <jbenson2008@gmail.com>
2021-10-20 20:57:23 -0400

    lesson 10

--- Lesson 10/04_StartupMatchmaker_Nav_Lab_Responsive/starter_code/js/navToggle.js
+++ Lesson 10/04_StartupMatchmaker_Nav_Lab_Responsive/starter_code/js/navToggle.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(event) {
 
+    document.getElementById('nav-toggle').addEventListener('click',function(){
+        document.querySelector('.main-nav').classList.toggle('main-nav-mobilehide');
+    })
 
 });
