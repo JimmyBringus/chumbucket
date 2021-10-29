ab0194f2f2313da8ae00612e1f6cd8f3f3fa3850
Jimmy B <jbenson2008@gmail.com>
2021-10-28 22:26:57 -0400

    final

--- Lesson 08/05_traffic_light/starter_code/script.js
+++ Lesson 08/05_traffic_light/starter_code/script.js
@@ -1,2 +1,26 @@
 // Try making the traffic light work - Think through the problem BEFORE you code!
 // You'll use addEventListener and getElementById if you're doing it right...
+
+let redLight = document.getElementById("stopButton");
+
+let yellowLight = document.getElementById("slowButton");
+
+let greenLight = document.getElementById("goButton");
+
+redLight.addEventListener("click",
+
+function(){
+    document.getElementById("stopLight").classList.add("red")
+});
+
+yellowLight.addEventListener("click",
+
+function(){
+    document.getElementById("slowLight").classList.add("yellow")
+});
+
+greenLight.addEventListener("click",
+
+function(){
+    document.getElementById("stopLight").classList.add("red")
+});
