916ab9b390befdba7977195a01a3f5a73667cdfa
Jimmy B <jbenson2008@gmail.com>
2021-11-24 17:31:51 -0500

    werk werk werk

--- Final_Project/js/js-staging.js
+++ Final_Project/js/js-staging.js
@@ -6,4 +6,20 @@ document.addEventListener('DOMContentLoaded', function(event) {
         e.preventDefault();
         darkmode.classList.toggle('bf-footer-click');
     })
-})
+});
+
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Form submitted, see you soon.')
+  })
+}
